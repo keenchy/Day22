1d72ee25c00ce08bd7d8a28b0ee3549d48795c2f
keenchy <h.kenan81@hotmail.com>
2021-03-29 22:07:36 +0200

    Created Plant Components React

---
+++ ex03/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Plant from './Plant';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "Plant" >
+        <
+        Plant / >
+        <
+        /div>
+    );
+}
+
+export default App;
