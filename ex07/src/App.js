36d0b9f5b4b600244cc4bc6be43284b059794dc2
keenchy <h.kenan81@hotmail.com>
2021-03-29 23:35:43 +0200

    Simple Box Content React

---
+++ ex07/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Box from './Box';
+import './App.css';
+
+function App() {
+    return ( <
+        h2 className = "Box" >
+        <
+        Box / >
+        <
+        /h2>
+    );
+}
+
+export default App;
---
+++ ex07/src/Box.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+class Box extends Component {
+    render() {
+        return ( <
+            h2 > This is a Box Component! < /h2>
+        )
+    }
+}
+
+export default Box;
