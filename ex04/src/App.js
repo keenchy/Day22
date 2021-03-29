73a4ee9d690a2b31cbc6db81677b955ff8030f20
keenchy <h.kenan81@hotmail.com>
2021-03-29 22:27:02 +0200

    Created a Box Class Component

---
+++ ex04/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Box from './Box';
+import './App.css';
+
+function App() {
+    return ( <
+        div className = "Box" >
+        <
+        Box / >
+        <
+        /div>
+    );
+}
+
+export default App;
